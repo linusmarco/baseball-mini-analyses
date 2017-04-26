181367e371aafd6a72051f942001333540b83e6c
Linus Marco <LMarco@mathematica-mpr.com>
2017-04-26 15:56:53 -0400

    first pass at data

--- matchup-vis/prepare_data.py
+++ matchup-vis/prepare_data.py
@@ -1,72 +1,80 @@
 import os
-import requests
 import numpy as np
 import pandas as pd
-import mlbgame
 
 
-def games_events(games):
-    all_events = []
-    for game in games:
-        events = mlbgame.events.game_events(game.game_id)
-        
-        for x in events:
-            for y in events[x]:
-                for i in events[x][y]:
-                    all_events.append(mlbgame.events.AtBat(i))
-
-        # for event in events:
-        #     all_events.append(event)
-    return all_events
+EVENT_DATA = "C:/Users/LMarco/Documents/02_Linus's Projects/RetroSheet/2016.CSV"
 
 
-def get_year(year):
-    teams = mlbgame.teams()
+def make_dummy(df, col):
+    df.loc[df[col] == 'T', col] = True
+    df.loc[df[col] == 'F', col] = False
+    assert df[col].isin([True, False]).all()
+    df[col] = pd.to_numeric(df[col])
 
-    all_games = []
 
-    for team in teams:
-        # for month in range(1, 13):
-        for month in [5]:
-            try:
-                # games = mlbgame.games(year, month, home=team.club_common_name)
-                games = mlbgame.games(year, month, 1, home=team.club_common_name)
-            except:
-                games = []
+def make_outcome_flags(df):
+    df['1B'] = df['EVENT_CD'] == 20
+    df['2B'] = df['EVENT_CD'] == 21
+    df['3B'] = df['EVENT_CD'] == 22
+    df['HR'] = df['EVENT_CD'] == 23
+    df['BB'] = df['EVENT_CD'] == 14
+    df['IBB'] = df['EVENT_CD'] == 15
+    df['HBP'] = df['EVENT_CD'] == 16
 
-            for game in games:
-                all_games.append(game)
 
-    return all_games
+def collapse_matchups(df, ids, vals):
+    d = df[ids + vals]
+    grouped = d.groupby(ids)
+    grouped = grouped.sum().reset_index()
+    return grouped
+
+
+def filter_mins(df, idvar, rank):
+    top = df.groupby(idvar).size().nlargest(rank).reset_index()[idvar]
+    df = df[df[idvar].isin(top)]
+    return df
 
 
 def main():
     
-    games_2015 = mlbgame.combine_games(get_year(2015))
-    print(len(games_2015))
+    # read event data
+    df = pd.read_csv(EVENT_DATA)
+
+    # clean up fields
+    make_dummy(df, 'BAT_EVENT_FL')
+    make_dummy(df, 'AB_FL')
+    make_dummy(df, 'SH_FL')
+    make_dummy(df, 'SF_FL')
+    df.rename(columns={
+        'RESP_BAT_ID': 'BAT_ID',
+        'RESP_PIT_ID': 'PIT_ID',
+        'AB_FL': 'AB',
+        'SH_FL': 'SH',
+        'SF_FL': 'SF'
+    }, inplace=True)
+
+    # filter to batting events (i.e no pickoffs, steals, etc.)
+    df = df[df['BAT_EVENT_FL']]
+    df.drop('BAT_EVENT_FL', axis=1, inplace=True)
+
+    # var creation
+    make_outcome_flags(df)
+
+    # collapse
+    ids = ['BAT_ID', 'PIT_ID']
+    vals = ['AB', '1B', '2B', '3B', 'HR', 'BB', 'IBB', 'HBP', 'SF']
+    df = df[df[vals].sum(axis=1) > 0]
+    matchups = collapse_matchups(df, ids, vals)    
+
+    # filter on minimums
+    matchups = filter_mins(matchups, 'BAT_ID', 200)
+    matchups = filter_mins(matchups, 'PIT_ID', 200)
+
+    print(matchups.info())
+    print(matchups.head())
     
-    events_2015 = games_events(games_2015)
-    print(len(events_2015))
-
-    fields = [
-        {
-            'batter': e.batter, 
-            'pitcher': e.pitcher,
-            'event': e.event
-        }
-    for e in events_2015]
-
-    df = pd.DataFrame(fields)
-
-    print(df.head())
-    print(df['event'].value_counts())
-
-
-    
-
 
 if (__name__ == "__main__"):
     main()
-
-
 
