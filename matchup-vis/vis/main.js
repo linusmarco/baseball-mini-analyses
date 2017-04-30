dd17ce1713e58fb713e4c3732028ecd46b96073c
Linus Marco <LMarco@mathematica-mpr.com>
2017-04-30 13:48:40 -0400

    basic force layout with data

--- matchup-vis/vis/main.js
+++ matchup-vis/vis/main.js
@@ -1,21 +1,92 @@
 var loadVis = function() {
     
+    console.log("hi");
+
     var svg = d3.select("svg"),
-        margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
 
-    var divisions = d3.scaleBand()
-        .rangeRound([0, height])
-        .paddingInner(0.1);
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-    var players = d3.scaleBand()
-        .padding(0.05);
+    var simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function(d) { return d.ID; }))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
 
-    d3.csv("../matchups.csv", function(data) {
-        console.log(data);
-    })
+    d3.json("data/data.json", function(error, graph) {
+        if (error) throw error;
+
+        graph.nodes = graph.nodes.filter(function(n) { return n.ID !== "OTHER" });
+        graph.links = graph.links.filter(function(l) { return l.source !== "OTHER" && l.target !== "OTHER" });
+
+        graph.nodes = graph.nodes.map(function(n) {
+            var node = n;
+            var group = node.POS === "BATTER" ? 1 : 2;
+            node.group = group;
+            return node;
+        });
+
+        console.log(graph);
+
+        var link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(graph.links)
+            .enter().append("line")
+                // .attr("stroke-width", function(d) { return Math.sqrt(d.OPS); });
+                .attr("stroke-width", function(d) { return 3 });
+
+        var node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(graph.nodes).enter()
+            .append("circle")
+                .attr("r", 5)
+                .attr("fill", function(d) { return color(d.group); })
+                .call(d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended));
+
+        node.append("title")
+            .text(function(d) { return d.ID; });
+
+        simulation
+            .nodes(graph.nodes)
+            .on("tick", ticked);
+
+        simulation.force("link")
+            .links(graph.links);
+
+        function ticked() {
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            node
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+        }
+    });
+
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
 }
 
 document.addEventListener('DOMContentLoaded', loadVis, false);
