8755c97610c2d7aacbea789300d88e56a11ab768
Linus Marco <lmarco163@gmail.com>
2017-05-09 21:07:28 -0400

    add script to grab data

---
+++ matchup-vis/get_data.py
@@ -0,0 +1,28 @@
+import os
+import urllib.request as rq
+import zipfile as zf
+
+
+RETROSHEET_EVENTS = "http://www.retrosheet.org/events/{}eve.zip"
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def download(url, file):
+    f = rq.urlretrieve(url, file)
+
+
+def main():
+    mkdir(DATA_DIR)
+
+    url = RETROSHEET_EVENTS.format(2016)
+    file = os.path.join(DATA_DIR, os.path.basename(url))
+    download(url, file)
+
+
+if (__name__ == "__main__"):
+    main()
